72a33d9f4133c8d9f9578c453b36e26d0f96ef2c
Giancarlo Paoli <giancarlopaoli@gmail.com>
2024-01-02 11:56:22 -0500

    tc

--- app/Models/ExchangeRate.php
+++ app/Models/ExchangeRate.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ClientComission;
 
 class ExchangeRate extends Model
 {
@@ -21,7 +22,8 @@ class ExchangeRate extends Model
         ];
     }
 
-    public function for_user(User $user=null, float $amount) : ExchangeRate {
+    //public function for_user(User $user=null, float $amount) : ExchangeRate {
+    public function for_user(User $user=null, float $amount) {
         
         if(!is_null($user)){
             $client = $user->assigned_client()->first();
@@ -35,14 +37,17 @@ class ExchangeRate extends Model
         $buy_spreads =[];
         $sell_spreads =[];
 
-        $general_spread = Range::where('min_range', '<=', $amount)
+        $ranges = Range::where('min_range', '<=', $amount)
             ->where('max_range', '>', $amount)
             ->where('active', true)
             ->first();
 
-        $general_spread = $market_closed ? $general_spread->spread_close : $general_spread->spread_open;
+        $general_spread = $market_closed ? $ranges->spread_close : $ranges->spread_open;
+        $general_spread_comission = $market_closed ? $ranges->comission_close : $ranges->comission_open;
         $buy_spreads[] = $general_spread;
         $sell_spreads[] = $general_spread;
+        $buy_spread_comissions[] = $general_spread_comission;
+        $sell_spread_comissions[] = $general_spread_comission;
 
         $vendor_ranges = VendorRange::where('min_range', '<=', $amount)
             ->where('max_range', '>', $amount)
@@ -67,21 +72,39 @@ class ExchangeRate extends Model
                 ->where('active', true)
                 ->latest()
                 ->first();
+
+            $client_comission = ClientComission::where('client_id', $client_id)
+                ->where('active', true)
+                ->latest()
+                ->first();
+
+            if(!is_null($client_comission)){
+                if(!$market_closed){
+                    $buy_spread_comissions[] = $client_comission->comission_open;
+                    $sell_spread_comissions[] = $client_comission->comission_open;
+                }
+                else{
+                    $buy_spread_comissions[] = $client_comission->comission_close;
+                    $sell_spread_comissions[] = $client_comission->comission_close;
+                }
+            }
+
         }
         else {
             $special_exchange_rate = null;
         }
 
+        $buy_spread_comission = min($buy_spread_comissions) / 10000.0;
+        $sell_spread_comission = min($sell_spread_comissions) / 10000.0;
 
         $exchange_rate = ExchangeRate::latest()->first();
         $user_exchange_rate = new ExchangeRate();
-        #$user_exchange_rate->id = $exchange_rate->id;
         $user_exchange_rate->created_at = $exchange_rate->created_at;
         $user_exchange_rate->updated_at = $exchange_rate->updated_at;
 
         if($special_exchange_rate == null) {
-            $user_exchange_rate->compra = round($exchange_rate->compra + $buy_spread, 4);
-            $user_exchange_rate->venta = round($exchange_rate->venta - $sell_spread, 4);
+            $user_exchange_rate->compra = round($exchange_rate->compra + $buy_spread + $buy_spread_comission, 4);
+            $user_exchange_rate->venta = round($exchange_rate->venta - $sell_spread - $sell_spread_comission, 4);
 
         } else {
             $user_exchange_rate->compra = round($special_exchange_rate->buying, 4);
