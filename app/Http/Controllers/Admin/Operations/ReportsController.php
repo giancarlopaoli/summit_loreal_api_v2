424e2e75f5332b8432d3f9f02792e7fd1e252750
Giancarlo Paoli <giancarlopaoli@gmail.com>
2024-01-31 14:21:53 -0500

    exchange rate comisiones especiales

--- app/Http/Controllers/Admin/Operations/ReportsController.php
+++ app/Http/Controllers/Admin/Operations/ReportsController.php
@@ -39,4 +39,38 @@ class ReportsController extends Controller
             ]
         ]);
     }
+
+    public function corfid(Request $request) {
+        $val = Validator::make($request->all(), [
+            'start_date' => 'required|date',
+            'end_date' => 'required|date'
+        ]);
+        if($val->fails()) return response()->json($val->messages());
+
+        $start_date = $request->start_date;
+        $end_date = $request->end_date;
+        
+        $buying_operations = Operation::select('id','operations.type','exchange_rate','currency_id','operation_status_id','operation_date')
+            ->selectRaw(" round(round(amount*exchange_rate,2) + comission_amount + igv,2) transfer_amount_pen")
+            ->selectRaw(" amount as  receipt_amount_usd")
+            ->selectRaw(" amount as  transfer_amount_usd")
+            ->selectRaw(" round(amount*exchange_rate,2) as  receipt_amount_pen")
+            //->selectRaw(" if(clients.customer_type ='PN', concat(name,' ',last_name, ' ', mothers_name), name ) as client_name")
+            //->join('clients', 'clients.id', '=', 'operations.client_id')
+            ->where('operations.type', 'Compra')
+            //->where('clients.type', 'Cliente')
+            ->whereIn('operation_status_id', [6,7,8])
+            ->whereRaw("date(operation_date) >= date('$start_date') and date(operation_date) <= date('$end_date')")
+            ->with('currency:id,name,sign')
+            ->with('status:id,name')
+            ->with('matches:id,client_id')
+            ->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'buying' => $buying_operations
+            ]
+        ]);
+    }
 }
