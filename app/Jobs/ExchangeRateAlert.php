7dd136c943f5a3065a27c3e064e9a4bb53f5f400
Giancarlo Paoli <giancarlopaoli@gmail.com>
2024-03-06 11:33:16 -0500

    TC alert

--- app/Jobs/ExchangeRateAlert.php
+++ app/Jobs/ExchangeRateAlert.php
@@ -8,10 +8,10 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use App\Models\Operation;
-use App\Models\OperationStatus;
+use App\Models\ExchangeRate;
+use Carbon\Carbon;
 
-class PendingDetractions implements ShouldQueue
+class ExchangeRateAlert implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -32,25 +32,17 @@ class PendingDetractions implements ShouldQueue
      */
     public function handle()
     {
-        $operations = Operation::where('operation_date', '>=', '2024-01-01')
-            ->where('operation_status_id', OperationStatus::where('name', 'Facturado')->first()->id)
-            ->whereRaw("( (detraction_amount > 0) or (type = 'Interbancaria' and currency_id = 2 and ( (comission_amount + igv) * exchange_rate > 690)))")
-            ->where('detraction_paid', false)
-            ->get();
+        $datatec = ExchangeRate::selectRaw(' (TIMESTAMPDIFF(MINUTE, created_at, now())) as tiempo_total')
+            ->latest()
+            ->first();
+        
+        $tiempo = $datatec->tiempo_total;
 
-        foreach ($operations as $op) {
-            $cliente =  $op->client->client_full_name;
-            $monto = number_format($op->amount,2);
-            $facturado = number_format($op->comission_amount + $op->igv,2);
-            $detraccion = number_format($op->detraction_amount,2);
-            $moneda = $op->currency->sign;
+        if($tiempo > 15) {
 
-            $message = "*Operaciones pendientes de Detracción*
-Cliente: *$cliente*
-Operación: $op->code
-Monto: $op->type $moneda$monto
-Monto Facturado: $facturado
-Detracción: $detraccion";
+            // Se envía notificación a Telegram
+            $message = "*Alerta Tipo Cambio DATATEC*
+El tipo de cambio no se ha actualizado desde hace *$tiempo * minutos";
         
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_POST, true);
@@ -58,14 +50,12 @@ Detracción: $detraccion";
             curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: multipart/form-data']);
             curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot1636692227:AAGHJX8dyPEKWGkvuti4Xfk84A0FlY1fIpk/sendMessage');
             $postFields = array(
-                'chat_id' => env('TELEGRAM_ADMIN_CHANNEL'),
+                'chat_id' => env('TELEGRAM_TI_CHANNEL'),
                 'text' => $message,
                 'parse_mode' => 'markdown',
                 'disable_web_page_preview' => false,
             );
             $rpta = curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-
-
             if(!curl_exec($ch))
                 echo curl_error($ch);
             curl_close($ch);
